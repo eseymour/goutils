ef65549f9167f59c77bcc2235621957388d54b7e
Edward Seymour <dev@edwardseymour.me>
2018-02-02 23:19:36 -0700

    Add decent documentation

--- cmd/fortuneStats/main.go
+++ cmd/fortuneStats/main.go
@@ -8,9 +8,14 @@ import (
 	"os/exec"
 )
 
+// The frequencies of byte values in fortunes.
 var byteFreqs [1 << 8]int
+
+// The distribution of byte values in fortunes.
 var byteDist [1 << 8]float64
 
+// Check prints an error message and exits with 1 if err != nil. It does nothing
+// otherwise.
 func check(err error) {
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error:", err)
@@ -22,41 +27,46 @@ func main() {
 	path, err := exec.LookPath("fortune")
 	check(err)
 
-	// Match on all fortune lists with regex matching anything
+	// Match on all fortune lists with regex matching anything.
 	cmd := exec.Command(path, "-am", ".*")
 	check(err)
 
 	cmdOut, err := cmd.StdoutPipe()
 	check(err)
-
 	check(cmd.Start())
 
-	count := 256 // Account for additive smoothing
+	// Account for additive smoothing.
+	count := 256
 	scanner := bufio.NewScanner(cmdOut)
 	for scanner.Scan() {
 		line := scanner.Bytes()
 
-		// Ignore strfile delimiter line
 		if bytes.Equal(line, []byte("%")) {
+			// Ignore strfile delimiter line.
 			continue
 		}
 
+		// Increment count and frequencies.
 		count += len(line)
 		for _, b := range line {
 			byteFreqs[b]++
 		}
 	}
 	check(scanner.Err())
+
+	// Fortune exits with one for some reason. Checking for that condition is too
+	// cumbersome, so we ignore Wait's return value.
 	cmd.Wait()
 
-	// Calculate distribution
+	// Calculate distribution.
 	for i := range byteFreqs {
-		// Additive smoothing
+		// Apply additive smoothing.
 		byteFreqs[i]++
 
 		byteDist[i] = float64(byteFreqs[i]) / float64(count)
 	}
 
+	// Print results in a form that can be inserted into a Go program.
 	fmt.Printf("var byteFreqs = %#v\n", byteFreqs)
 	fmt.Printf("var byteDist = %#v\n", byteDist)
 }
