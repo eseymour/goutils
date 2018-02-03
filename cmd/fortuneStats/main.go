6583d071d87bcd126a4b314e25285e72d8f1cb4d
Edward Seymour <dev@edwardseymour.me>
2018-02-02 22:05:08 -0700

    Add fortuneStat stub

---
+++ cmd/fortuneStats/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	_, err := exec.LookPath("fortune")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		os.Exit(1)
+	}
+}
