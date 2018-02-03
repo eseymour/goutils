c056adc7e7a7a1b859fc2bb7f47484202933ccde
Edward Seymour <dev@edwardseymour.me>
2018-02-02 22:52:30 -0700

    Add byte frequency tally

--- cmd/fortuneStats/main.go
+++ cmd/fortuneStats/main.go
@@ -1,15 +1,51 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
 )
 
-func main() {
-	_, err := exec.LookPath("fortune")
+var byteFreqs [1 << 8]int
+var byteDist [1 << 8]float64
+
+func check(err error) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		fmt.Fprintln(os.Stderr, "Error:", err)
 		os.Exit(1)
 	}
+}
+
+func main() {
+	path, err := exec.LookPath("fortune")
+	check(err)
+
+	// Match on all fortune lists with regex matching anything
+	cmd := exec.Command(path, "-am", ".*")
+	check(err)
+
+	cmdOut, err := cmd.StdoutPipe()
+	check(err)
+
+	check(cmd.Start())
+
+	scanner := bufio.NewScanner(cmdOut)
+	for scanner.Scan() {
+		line := scanner.Bytes()
+
+		// Ignore strfile delimiter line
+		if bytes.Equal(line, []byte("%")) {
+			continue
+		}
+
+		for _, b := range line {
+			byteFreqs[b]++
+		}
+	}
+	check(scanner.Err())
+	cmd.Wait()
+
+	fmt.Printf("var byteFreqs = %#v\n", byteFreqs)
 }
