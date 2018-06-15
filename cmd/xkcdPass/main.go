3603c890ba3562254645ca9ff8158aa0062b8801
Edward Seymour <dev@eseymour.me>
2018-06-14 22:36:42 -0600

    Use crypto/rand instead of math/rand because we are generating passwords

--- cmd/xkcdPass/main.go
+++ cmd/xkcdPass/main.go
@@ -2,29 +2,25 @@ package main
 
 import (
 	"bufio"
+	"crypto/rand"
 	"flag"
 	"fmt"
 	"math"
-	"math/rand"
+	"math/big"
 	"os"
-	"time"
 	"unicode"
 	"unicode/utf8"
 )
 
 var (
-	numPass  int
-	numWords int
-	verbose  bool
+	passCount int
+	wordCount int
+	verbose   bool
 )
 
 func init() {
-	// Seed RNG since it is not normally seeded
-	rand.Seed(time.Now().Unix())
-
-	// Basic flags
-	flag.IntVar(&numPass, "p", 1, "number of passwords to generate")
-	flag.IntVar(&numWords, "n", 4, "number of words per password")
+	flag.IntVar(&passCount, "p", 1, "number of passwords to generate")
+	flag.IntVar(&wordCount, "w", 4, "number of words per password")
 	flag.BoolVar(&verbose, "v", false, "verbose output to stderr")
 	flag.Parse()
 }
@@ -33,7 +29,7 @@ func main() {
 	// Check common words file locations
 	f, err := os.Open("/usr/share/dict/words")
 	if err != nil {
-		// Try again
+		// Try the other common location.
 		f, err = os.Open("/usr/dict/words")
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Unable to find a words file.")
@@ -42,30 +38,39 @@ func main() {
 	}
 	s := bufio.NewScanner(f)
 
-	// Load words into slice for random access
-	var words []string
+	// Load dictionary into slice for random access.
+	var dict []string
 	for s.Scan() {
-		word := s.Text()
+		w := s.Text()
 
-		// Avoid proper words and numbers
-		first, _ := utf8.DecodeRuneInString(word)
+		// Avoid using proper nouns and numbers.
+		first, _ := utf8.DecodeRuneInString(w)
 		if unicode.IsLower(first) {
-			words = append(words, word)
+			dict = append(dict, w)
 		}
 	}
 
-	// Password generation
-	for i := 0; i < numPass; i++ {
-		for j := 0; j < numWords; j++ {
-			fmt.Printf("%s ", words[rand.Intn(len(words))])
+	// Store dictionary length in a big.Int for use in the CSPRNG.
+	var dictLen big.Int
+	dictLen.SetInt64(int64(len(dict)))
+
+	// Generate passwords using the crypto/rand CSPRNG.
+	for i := 0; i < passCount; i++ {
+		for j := 0; j < wordCount; j++ {
+			n, err := rand.Int(rand.Reader, &dictLen)
+			if err != nil {
+				fmt.Fprint(os.Stderr, "CSPRNG error: ", err)
+			}
+
+			fmt.Printf("%s ", dict[n.Int64()])
 		}
 		fmt.Println()
 	}
 
-	// Entropy calculation
+	// Calculate the number of bits of entropy for a generated password.
 	if verbose {
-		entropy := math.Round(math.Log2(float64(len(words))) * float64(numWords))
+		entropy := math.Round(math.Log2(float64(len(dict))) * float64(wordCount))
 		fmt.Fprintf(os.Stderr, "Generated %d password(s) with ~%.f bits of entropy each.\n",
-			numPass, entropy)
+			passCount, entropy)
 	}
 }
